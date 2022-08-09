d4af54424ae02f8f0954ba8727b37d0958617640
Bradley Bellairs <bwbellairs@outlook.com>
2022-08-09 22:39:31 +0100

    Add tests for /api/submit endpoint
    Signed-off-by: Bradley Bellairs <bwbellairs@outlook.com>

--- tests/controllers/rootController/rootController.test.ts
+++ tests/controllers/rootController/rootController.test.ts
@@ -15,6 +15,25 @@ describe('Root controller', () => {
     expect(response.statusCode).toEqual(200);
     expect(response.body).toEqual({message: 'Hello: tester'});
   });
+
+  it('/api/submit returns 200 when a valid youtube url is provided', async () => {
+    init(app);
+    const response = await request(app).get('/api/submit').query({url: 'https://www.youtube.com/watch?v=7rqEB4xNZU4'});
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.id).toBeDefined();
+  });
+
+  it('/api/submit returns 400 when no input is provided', async () => {
+    init(app);
+    const response = await request(app).get('/api/submit');
+    expect(response.statusCode).toEqual(400);
+  });
+
+  it('/api/submit returns 400 when bad input is provided', async () => {
+    init(app);
+    const response = await request(app).get('/api/submit').query({url: 'octopus go brrrrrr'});
+    expect(response.statusCode).toEqual(400);
+  });
 });
 
 export default {};
