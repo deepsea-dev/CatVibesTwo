9904c93e3fdb50b2e55b3ee5e8e930045a0470e9
Bradley Bellairs <11426553+BWBellairs@users.noreply.github.com>
2022-08-23 15:52:29 +0100

    Created api endpoint to receive youtube url (#2)
    A new route has been created that allows submission of url's.
    These are checked against a regex that pulls out a YouTube video id after validation.
    There is a placeholder currently for returning the new video id that'll be stored in backend.

--- src/controllers/rootController/rootController.ts
+++ src/controllers/rootController/rootController.ts
@@ -2,11 +2,27 @@
 // api/jobs which returns the status of jobs and allows new jobs to be submitted for example called JobController
 // and another maybe for api/download which maybe is called DownloadController and allows the user to download the processed video
 
-import express from 'express';
+import { Application, Request, Response } from 'express';
+import { DownloadMessageRequest } from '../../models/downloadMessageRequest';
+import { DownloadMessageResponse } from '../../models/downloadMessageResponse';
 import { HelloMessageResponse } from '../../models/helloMessageResponse';
+import { ErrorResponse } from '../../models/ErrorResponse';
 
-export const init = (app: express.Application): void => {
-  app.get('/api', async (req, res: express.Response<HelloMessageResponse>) => {
+const YoutubeURLRegex = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\\-]+\?v=|embed\/|v\/)?)([\w\\-]+)(\S+)?$/;
+
+export const init = (app: Application): void => {
+  app.get('/api', async (req, res: Response<HelloMessageResponse>) => {
     res.status(200).json({message: `Hello: ${req.headers['user-agent']}`});
   });
+  app.post('/api/submit', async (req: Request<DownloadMessageRequest>, res: Response<DownloadMessageResponse | ErrorResponse>) => {
+    if (req.body.url === undefined) return res.status(400).json({ message: 'No url provided' });
+    
+    const videoID = req.body.url.match(YoutubeURLRegex);
+    if (!videoID || videoID.length < 6) return res.status(400).send({ message: 'No video id provided or could not be found' });
+
+    const newVideoID = 'placeholder';
+    return res.status(200).json({id: newVideoID});
+
+  }); 
+  
 };
