a78f0c07cfaf648a49847a7a1e36f1f148f3ab97
Bradley Bellairs <bwbellairs@outlook.com>
2022-08-23 10:52:13 +0100

    Remove content type and accept headers from test files
    Signed-off-by: Bradley Bellairs <bwbellairs@outlook.com>

--- tests/controllers/rootController/rootController.test.ts
+++ tests/controllers/rootController/rootController.test.ts
@@ -19,30 +19,23 @@ describe('Root controller', () => {
 
   it('/api/submit returns 200 when a valid youtube url is provided', async () => {
     init(app);
-    await request(app).post('/api/submit')
-      .send({ url: 'https://www.youtube.com/watch?v=7rqEB4xNZU4' })
-      .set('Content-Type', 'application/json')
-      .set('Accept', 'application/json')
-      .expect(200)
-      .then((response) => {
-        expect(response).toBeDefined();
-      });
+    const response = await request(app).post('/api/submit')
+      .send({ url: 'https://www.youtube.com/watch?v=7rqEB4xNZU4' });
+    expect(response.statusCode).toEqual(200);
+      
   });
 
   it('/api/submit returns 400 when no input is provided', async () => {
     init(app);
-    await request(app).post('/api/submit')
-      .set('Content-Type', 'application/json')
-      .set('Accept', 'application/json')
-      .expect(400);
+    const response = await request(app).post('/api/submit');
+    expect(response.statusCode).toEqual(400);
+  
   });
 
   it('/api/submit returns 400 when bad input is provided', async () => {
     init(app);
-    await request(app).post('/api/submit').send({ url: 'octopus go brrrrrr' })
-      .set('Content-Type', 'application/json')
-      .set('Accept', 'application/json')
-      .expect(400);
+    const response = await request(app).post('/api/submit').send({ url: 'octopus go brrrrrr' });
+    expect(response.statusCode).toEqual(400);    
   });
 });
 
