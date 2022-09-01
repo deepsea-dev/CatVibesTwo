7b459577d63cc7ed762bc5304a27b62b075ea948
Bradley Bellairs <bwbellairs@outlook.com>
2022-09-01 21:54:29 +0100

    Add background image

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,25 @@
 import { UrlEntry } from './components/urlEntry';
 
+import backgroundImage from './images/background.png';
+import styled from 'styled-components';
+
+const Content = styled.div`
+  background-image: url(${backgroundImage});
+  background-position: 70% 50%;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  /* filter: blur(8px); */
+  width: 70%;
+  height: 100vh;
+`;
+
 function App() {
   return (
-    <UrlEntry/>
+    <Content>
+      <UrlEntry/>
+    </Content>
   );
 }
 
 export default App;
+
